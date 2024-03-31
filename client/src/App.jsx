2265a644660f1c2a8f0efb347dcc9c720c07aa0d
omitewary <omitewary@gmal.com>
2024-03-31 09:37:11 -0400

    added header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Header from "./pages/Header";
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      <h1>hello</h1>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
